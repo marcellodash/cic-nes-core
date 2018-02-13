751ed96ce0fc41b2974e78da34d3a2a68ee74ef0
rene richard <rene@db-electronics.ca>
2018-02-12 22:09:08 -0500

    added ram and rom blocks, adi-skai-lbli-ldi now decoded

--- src/cic.vhd
+++ src/cic.vhd
@@ -60,6 +60,7 @@ entity cic is
 		
 		--Test
 		Accumulator_p	:  out	std_logic_vector(4 downto 0);
+		Rom_p				:  out	std_logic_vector(7 downto 0);
 		PC_p				:  out 	std_logic_vector(9 downto 0)
 	);
 end entity; 
@@ -73,8 +74,8 @@ architecture cic_a of cic is
 --+-+         +-+-+-+-+  +- - - - - - - - - - - - - - - -+
 --            |   X   |  |       |       |       |       |
 --        +---+-+-+-+-+  +- - - - - - - - - - - - - - - -+
---        |     P     |  |       |       |       |       |
---        | PH|   PL  |  +- - - - - - - - - - - - - - - -+
+--        |     B     |  |       |       |       |       |
+--        | BH|   BL  |  +- - - - - - - - - - - - - - - -+
 --+-------+-+-+-+-+-+-+  |       |       |       |       |
 --|         IC        |  +- - - - - - - -R- - - - - - - -+
 --+-+-+-+-+-+-+-+-+-+-+  |       |       |       |       |
@@ -91,8 +92,8 @@ architecture cic_a of cic is
 --C  = Carry flag
 --X  = 4-bit General register
 --P  = Pointer, used for memory access
---PH = Upper 2-bits of P
---PL = Lower 4-bits of P, used for I/O
+--BH = Upper 2-bits of P
+--BL = Lower 4-bits of P, used for I/O
 --IC = Instruction counter, to save some space; it counts in a polynominal manner instead of linear manner
 --S  = Stack for the IC register
 --R  = 32 nibbles of RAM
@@ -100,7 +101,7 @@ architecture cic_a of cic is
 
 -- types
 	--FSM
-	type cpu_cycles_type IS (cpuLoad, cpuRead, cpuModify, cpuWrite, cpuSkip);
+	type cpu_cycles_type IS (LD, RD, MD, WR, IDLE);
 	
 	--stack 
 	type cic_stack_typ is array (0 to 3) of std_logic_vector(9 downto 0);
@@ -108,13 +109,17 @@ architecture cic_a of cic is
 -- exposed registers and signals
 	signal carry_s				: std_logic;
 	signal acc_s				: std_logic_vector(4 downto 0);
-	signal xreg_s				: std_logic_vector(3 downto 0);
-	signal ptr_s				: std_logic_vector(5 downto 0);
+	signal x_s					: std_logic_vector(3 downto 0);
+	signal b_s					: std_logic_vector(5 downto 0);
 	signal pc_s					: std_logic_vector(9 downto 0);
 	
 -- internal
 	signal temp_s				: std_logic_vector(4 downto 0);
-	signal current_op_s		: std_logic_vector(7 downto 0);
+	signal opcode_s			: std_logic_vector(7 downto 0);
+	signal rom_s				: std_logic_vector(7 downto 0);
+	signal ramin_s				: std_logic_vector(3 downto 0);
+	signal ramout_s			: std_logic_vector(3 downto 0);
+	signal wr_ram_s			: std_logic;
 	
 	signal temp_pc_s			: std_logic_vector(9 downto 0);
 	signal stack_s				: cic_stack_typ;
@@ -123,6 +128,7 @@ architecture cic_a of cic is
 	signal inc_pc_s			: std_logic;
 	signal load_pc_s			: std_logic;
 	signal skip_s				: std_logic;
+	
 	
 	
 begin
@@ -148,47 +154,69 @@ begin
 --00101111
 --01010111
 
+	--memory file must be manually copied to simulation folder else an error will occur during simulation
+	rom_inst: entity work.rom
+		port map(
+			address 	=> pc_s,
+			clock 	=> Clk_p,
+			q 			=> rom_s
+		);
+	
+	-- ram
+	ram_inst: entity work.ram
+		port map
+		(
+			address 	=> b_s,
+			clock 	=> Clk_p,
+			data		=> ramin_s,
+			wren		=> wr_ram_s,
+			q			=> ramout_s
+		);
+
+	
 	--external signals
 	PC_p <= pc_s;
 	Accumulator_p <= acc_s;
+	Rom_p <= rom_s;
 
+	--cycle through the four cpu states, this is a 4 cycle cpu
 	cpu_state: process( Clk_p, Reset_p )
 	begin
 		if Reset_p = '0' then
-			cpu_state_s <= cpuLoad;
-			cpu_next_state_s <= cpuRead;
+			cpu_state_s <= LD;
+			cpu_next_state_s <= RD;
 		elsif (rising_edge(Clk_p)) then
 			case cpu_state_s is
 				--Q1
-				when cpuLoad =>
-					cpu_state_s <= cpuRead;
-					cpu_next_state_s <= cpuModify;
+				when LD =>
+					cpu_state_s <= RD;
+					cpu_next_state_s <= MD;
 				--Q2
-				when cpuRead =>
-					cpu_state_s <= cpuModify;
-					cpu_next_state_s <= cpuWrite;
+				when RD =>
+					cpu_state_s <= MD;
+					cpu_next_state_s <= WR;
 				--Q3
-				when cpuModify =>
-					cpu_state_s <= cpuWrite;
-					cpu_next_state_s <= cpuLoad;
+				when MD =>
+					cpu_state_s <= WR;
+					cpu_next_state_s <= LD;
 				--Q4
-				when cpuWrite =>
-					cpu_state_s <= cpuLoad;
-					cpu_next_state_s <= cpuRead;
+				when WR =>
+					cpu_state_s <= LD;
+					cpu_next_state_s <= RD;
 				--Skip states
-				when cpuSkip =>
-					cpu_state_s <= cpuLoad;
+				when IDLE =>
+					null;
 			end case;
 		end if;
 	end process;
 	
+	--the program counter is a polynomial counter, it increments by setting bit6 only if bit1 and bit0 are equal
+	--this process also loads the current opcode into a register
 	program_counter: process( Clk_p, Reset_p )
-		
 	begin
-		
 		if Reset_p = '0' then
 			pc_s <= ( others => '0');
-			current_op_s <= ( others => '0');
+			opcode_s <= ( others => '0');
 		elsif (rising_edge(Clk_p)) then
 			if inc_pc_s = '1' then
 				pc_s(5 downto 0) <= pc_s(6 downto 1);
@@ -201,12 +229,14 @@ begin
 				pc_s <= temp_pc_s;
 			end if;
 			
-			if cpu_next_state_s = cpuLoad then
+			if cpu_next_state_s = LD then
 				-- insert a NOP for skip
 				if skip_s = '1' then
-					current_op_s <= ( others => '0');
+					opcode_s <= ( others => '0');
+					
+				--else load next instruction from rom
 				else
-					current_op_s <= current_op_s + X"01";
+					opcode_s <= rom_s;
 				end if;
 			end if;
 		else
@@ -217,7 +247,6 @@ begin
 	
 
 	opcodes: process( Clk_p, Reset_p )
-
 	begin
 	
 		if Reset_p = '0' then
@@ -225,6 +254,8 @@ begin
 			inc_pc_s <= '0';
 			load_pc_s <= '0';
 			acc_s <= ( others => '0');
+			b_s <= ( others => '0');
+			x_s <= ( others => '0');
 			temp_s <= ( others => '0');
 			
 		elsif ( rising_edge(Clk_p)) then
@@ -233,17 +264,18 @@ begin
 			skip_s <= '0';
 			
 			-- decode operations
-			case current_op_s(7 downto 4) is
+			case opcode_s(7 downto 4) is
 			
-				-- adi N "add immediate", acc = acc +N, skip if overflow
+				-- adi N (00+N)
+				-- add immediate, acc = acc + N, skip if overflow
 				-- 00 is NOP
 				when x"0" =>
 					case cpu_next_state_s is
-						when cpuRead =>
+						when RD =>
 							temp_s <= ('0' & acc_s(3 downto 0));
-						when cpuModify =>
-							temp_s <= temp_s + ('0' & current_op_s(3 downto 0));
-						when cpuWrite =>
+						when MD =>
+							temp_s <= temp_s + ('0' & opcode_s(3 downto 0));
+						when WR =>
 							acc_s <= temp_s;
 							inc_pc_s <= '1';
 							if( temp_s(4) = '1' ) then
@@ -253,25 +285,52 @@ begin
 							null;
 					end case;
 					
-				-- skai "skip accumulator immediate"
+				-- skai N (10+N)
+				-- skip accumulator immediate, skip if acc = N 
 				when x"1" =>
 					case cpu_next_state_s is
-						when cpuRead =>
+						when RD =>
 							temp_s <= acc_s;
-						when cpuModify =>
-							null;
-						when cpuWrite =>
-							if( temp_s = current_op_s(3 downto 0) ) then
+						when WR =>
+							if( temp_s = opcode_s(3 downto 0) ) then
 								skip_s <= '1';
 							end if;
+							inc_pc_s <= '1';
 						when others =>
 							null;
 					end case;
 					
-				-- lbli " 
+				-- lbli N (20+N)
+				-- load B low immediate, BL = N
+				when x"2" =>
+					case cpu_next_state_s is
+						when WR =>
+							b_s(3 downto 0) <= opcode_s(3 downto 0);
+							inc_pc_s <= '1';
+						when others =>
+							null;
+					end case;
 				
+				-- ldi N (30+N)
+				-- load immediate, A = N
+				when x"3" =>
+					case cpu_next_state_s is
+						when WR =>
+							acc_s <= ('0' & opcode_s(3 downto 0));
+							inc_pc_s <= '1';
+						when others =>
+							null;
+					end case;
+					
+				-- unrecognized opcode
 				when others =>
-					null;
+					case cpu_next_state_s is
+						when WR =>
+							skip_s <= '1';
+							inc_pc_s <= '1';
+						when others =>
+							null;
+					end case;
 				
 			end case;
 		end if;
